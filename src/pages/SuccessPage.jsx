0b1e55866ec5aaff74f3fd1a49c312d5d222b19d
Soedjamal <akmalihksan181@gmail.com>
2025-02-19 09:17:17 +0700

    add: button back

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const SuccessPage = () => {
   return (
@@ -20,26 +21,41 @@ const SuccessPage = () => {
         }}
       >
         <img
-          style={{ width: "150px", height: "170px" }}
+          style={{ width: "180px", height: "200px" }}
           src="https://ik.imagekit.io/nir38m3b3/20250213_174211_0000.png?updatedAt=1739443422808"
           alt=""
         />
-        <Typography
-          style={
-            {
-              // border: "1px solid black",
+        <Box>
+          <Typography
+            style={
+              {
+                // border: "1px solid black",
+              }
             }
-          }
-          variant="h4"
-          fontWeight="700"
-          color="#006787"
-        >
-          Berhasil memilih.👀
-        </Typography>
+            variant="h4"
+            fontWeight="700"
+            color="#006787"
+          >
+            Berhasil memilih.👀
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "start", flexDirection: "column" }} >
+            <Typography sx={{ fontSize: "1.2rem", mt: 3 }} fontWeight="500" color="rgb(130,130,130)" textAlign="center">
+              "Terimakasih telah memilih"
+
+            </Typography>
+            <Link style={{
+              padding: "12px 35px",
+              backgroundColor: "#006787",
+              borderRadius: "10px",
+              color: "rgb(200,200,200)",
+              margin: "20px 0",
+              textDecoration: "none",
+              fontWeight: "700",
+            }} to="/">Kembali</Link>
+          </Box>
+        </Box>
       </div>
-      <Typography fontWeight="500" color="rgb(130,130,130)" textAlign="center">
-        "Terimakasih telah memilih"
-      </Typography>
+
     </div>
   );
 };
