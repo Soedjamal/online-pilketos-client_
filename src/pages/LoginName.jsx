b4e2a799592b3e4b201f18628c464893b7da4d88
Soedjamal <akmalihksan181@gmail.com>
2025-02-20 07:17:32 +0700

    fix: placeholder input name

--- src/pages/LoginName.jsx
+++ src/pages/LoginName.jsx
@@ -124,7 +124,7 @@ const LoginName = () => {
           </Typography>
           <TextField
             fullWidth
-            label="Contoh Penulisan: Ahmad Wahid Baharuddin"
+            label="Contoh Penulisan: Ahmad Wahid Baharudin"
             variant="outlined"
             style={{ outlineColor: "#006787" }}
             value={name}
